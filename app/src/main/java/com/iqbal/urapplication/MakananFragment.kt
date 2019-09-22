782863a37f11a9e68436d0edb7b192d7a82e8aef
Iqbal <muhamadi1000@gmail.com>
2019-09-22 10:58:41 +0700

    Initial commit

---
+++ app/src/main/java/com/iqbal/urapplication/MakananFragment.kt
@@ -0,0 +1,24 @@
+package com.iqbal.urapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MakananFragment: Fragment() {
+
+    companion object{
+        fun getInstance():MakananFragment{
+            return MakananFragment()
+        }
+    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.makanan_fragment,
+            container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
